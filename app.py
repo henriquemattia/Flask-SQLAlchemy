d49315a108cf547733e1659f80bc5369a6a9e34f
henriquemattia <mattia.henrique@gmail.com>
2022-10-18 16:45:24 -0300

    teste

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ api.add_resource(ProductsCalcado, "/calcados")
 api.add_resource(ProductsAcessorios, "/acessorios")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000,)
+    app.run()
+    # app.run(debug=True, port=5000,)
